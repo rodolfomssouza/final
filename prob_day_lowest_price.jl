dac6d20e3ed4132f1b98d0ad39890dfc586d8c7b
Rodolfo Souza <souzarod@proton.me>
2024-07-05 23:17:14 -0500

    add base functions
    add base functions to calculate the probability of lowest price

--- prob_day_lowest_price.jl
+++ prob_day_lowest_price.jl
@@ -64,6 +64,49 @@ function get_market_data(asset::Symbol)
     return df
 end
 
+function merge_data_with_continous_dates(df)
+    """
+    Create a continuous date range
+    """
+    d0 = first(df.date)
+    d1 = last(df.date)
+    dates = []
+    for i = d0:d1
+        push!(dates, i)
+    end
+    df_dates = DataFrame(date = dates)
+    # Merge the dataframes
+    df = outerjoin(df_dates, df, on = :date)
+    # Sort by date
+    df = sort(df, :date)
+    return df
+end
+
+
+function fill_missing_data(df)
+    """
+    If data is missing, fill with next available data
+    """
+    for i = 1:size(df, 1)
+        if ismissing(df.Low[i])
+            df.Low[i] = df.Low[i + 1]
+        end
+        if ismissing(df.High[i])
+            df.High[i] = df.High[i + 1]
+        end
+        if ismissing(df.Open[i])
+            df.Open[i] = df.Open[i + 1]
+        end
+        if ismissing(df.Close[i])
+            df.Close[i] = df.Close[i + 1]
+        end
+        if ismissing(df.Volume[i])
+            df.Volume[i] = df.Volume[i + 1]
+        end
+    end
+    return df
+end
+
 
 function main(asset::Symbol)
     """
